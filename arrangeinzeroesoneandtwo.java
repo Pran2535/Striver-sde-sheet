d78618ff506579b0d17eb003e6f87377f00a736c
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2024-01-06 11:33:21 +0530

    Create arrangeinzeroesoneandtwo.java

---
+++ arrangeinzeroesoneandtwo.java
@@ -0,0 +1,10 @@
+package arrays;
+import java.util.*;
+public class sortarraysofzero1and2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int arr[]= {1,0,0,1,2,2,0,3,3};
+     Array.sort(arr);
+  }
+}
